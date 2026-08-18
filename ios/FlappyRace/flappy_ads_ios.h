2ae984ae64d39a76945d36284e49219c1c215e0b
Sergio Lozano Garza <slgarza@me.com>
2026-08-18 14:41:31 -0600

    Add iOS remove ads purchase

--- ios/FlappyRace/flappy_ads_ios.h
+++ ios/FlappyRace/flappy_ads_ios.h
@@ -10,20 +10,32 @@ class FlappyAdsIOS : public Object {
 
 	void *_delegate = nullptr;
 	void *_interstitial = nullptr;
+	void *_product_request = nullptr;
+	void *_remove_ads_product = nullptr;
 	bool _initializing = false;
 	bool _initialized = false;
 	bool _loading_interstitial = false;
+	bool _remove_ads_owned = false;
+	bool _loading_remove_ads_product = false;
 
 public:
 	void initialize();
 	void loadInterstitial();
 	bool isInterstitialReady() const;
 	bool showInterstitial();
+	void purchase_remove_ads();
+	void restore_purchases();
+	bool has_remove_ads() const;
 
 	void _on_interstitial_loaded(void *p_interstitial);
 	void _on_interstitial_failed();
 	void _on_interstitial_closed();
 	void _on_interstitial_shown();
+	void _on_remove_ads_products_response(void *p_products);
+	void _on_remove_ads_product_request_failed();
+	void _on_remove_ads_purchased();
+	void _on_remove_ads_purchase_failed();
+	void _on_remove_ads_purchase_cancelled();
 
 	FlappyAdsIOS();
 	~FlappyAdsIOS();
--- ios/FlappyRace/flappy_ads_ios.mm
+++ ios/FlappyRace/flappy_ads_ios.mm
@@ -6,12 +6,14 @@
 
 #import <Foundation/Foundation.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
+#import <StoreKit/StoreKit.h>
 #import <UIKit/UIKit.h>
 
 static NSString *const kFlappyAdsInterstitialAdUnitID = @"ca-app-pub-5520658565266027/6038612501";
+static NSString *const kFlappyAdsRemoveAdsProductID = @"remove_ads_flappykart";
 static FlappyAdsIOS *flappy_ads_singleton = nullptr;
 
-@interface FlappyAdsDelegate : NSObject <GADFullScreenContentDelegate>
+@interface FlappyAdsDelegate : NSObject <GADFullScreenContentDelegate, SKProductsRequestDelegate, SKPaymentTransactionObserver>
 - (instancetype)initWithOwner:(FlappyAdsIOS *)owner;
 @end
 
@@ -48,6 +50,62 @@ static FlappyAdsIOS *flappy_ads_singleton = nullptr;
 	}
 }
 
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
+	if (_owner) {
+		_owner->_on_remove_ads_products_response((__bridge void *)response.products);
+	}
+}
+
+- (void)request:(SKRequest *)request didFailWithError:(NSError *)error {
+	NSLog(@"FlappyAds: remove ads product request failed: %@", error.localizedDescription);
+	if (_owner) {
+		_owner->_on_remove_ads_product_request_failed();
+	}
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions {
+	for (SKPaymentTransaction *transaction in transactions) {
+		if (![transaction.payment.productIdentifier isEqualToString:kFlappyAdsRemoveAdsProductID]) {
+			continue;
+		}
+
+		switch (transaction.transactionState) {
+			case SKPaymentTransactionStatePurchased:
+			case SKPaymentTransactionStateRestored:
+				if (_owner) {
+					_owner->_on_remove_ads_purchased();
+				}
+				[queue finishTransaction:transaction];
+				break;
+			case SKPaymentTransactionStateFailed:
+				if (_owner) {
+					if (transaction.error.code == SKErrorPaymentCancelled) {
+						_owner->_on_remove_ads_purchase_cancelled();
+					} else {
+						NSLog(@"FlappyAds: remove ads purchase failed: %@", transaction.error.localizedDescription);
+						_owner->_on_remove_ads_purchase_failed();
+					}
+				}
+				[queue finishTransaction:transaction];
+				break;
+			case SKPaymentTransactionStatePurchasing:
+			case SKPaymentTransactionStateDeferred:
+				break;
+		}
+	}
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue restoreCompletedTransactionsFailedWithError:(NSError *)error {
+	NSLog(@"FlappyAds: restore purchases failed: %@", error.localizedDescription);
+	if (_owner) {
+		if (error.code == SKErrorPaymentCancelled) {
+			_owner->_on_remove_ads_purchase_cancelled();
+		} else {
+			_owner->_on_remove_ads_purchase_failed();
+		}
+	}
+}
+
 @end
 
 void FlappyAdsIOS::_bind_methods() {
@@ -55,11 +113,17 @@ void FlappyAdsIOS::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("loadInterstitial"), &FlappyAdsIOS::loadInterstitial);
 	ClassDB::bind_method(D_METHOD("isInterstitialReady"), &FlappyAdsIOS::isInterstitialReady);
 	ClassDB::bind_method(D_METHOD("showInterstitial"), &FlappyAdsIOS::showInterstitial);
+	ClassDB::bind_method(D_METHOD("purchase_remove_ads"), &FlappyAdsIOS::purchase_remove_ads);
+	ClassDB::bind_method(D_METHOD("restore_purchases"), &FlappyAdsIOS::restore_purchases);
+	ClassDB::bind_method(D_METHOD("has_remove_ads"), &FlappyAdsIOS::has_remove_ads);
 
 	ADD_SIGNAL(MethodInfo("interstitial_loaded"));
 	ADD_SIGNAL(MethodInfo("interstitial_failed"));
 	ADD_SIGNAL(MethodInfo("interstitial_shown"));
 	ADD_SIGNAL(MethodInfo("interstitial_closed"));
+	ADD_SIGNAL(MethodInfo("remove_ads_purchased"));
+	ADD_SIGNAL(MethodInfo("remove_ads_purchase_failed"));
+	ADD_SIGNAL(MethodInfo("remove_ads_purchase_cancelled"));
 }
 
 void FlappyAdsIOS::initialize() {
@@ -79,6 +143,14 @@ void FlappyAdsIOS::initialize() {
 }
 
 void FlappyAdsIOS::loadInterstitial() {
+	if (_remove_ads_owned) {
+		if (_interstitial != nullptr) {
+			CFRelease(_interstitial);
+			_interstitial = nullptr;
+		}
+		return;
+	}
+
 	if (_loading_interstitial || _interstitial != nullptr) {
 		return;
 	}
@@ -112,6 +184,11 @@ bool FlappyAdsIOS::isInterstitialReady() const {
 }
 
 bool FlappyAdsIOS::showInterstitial() {
+	if (_remove_ads_owned) {
+		emit_signal("interstitial_closed");
+		return true;
+	}
+
 	if (_interstitial == nullptr) {
 		loadInterstitial();
 		return false;
@@ -133,6 +210,54 @@ bool FlappyAdsIOS::showInterstitial() {
 	});
 
 	return true;
+}
+
+void FlappyAdsIOS::purchase_remove_ads() {
+	if (_remove_ads_owned) {
+		emit_signal("remove_ads_purchased");
+		return;
+	}
+
+	if (![SKPaymentQueue canMakePayments]) {
+		NSLog(@"FlappyAds: StoreKit payments are disabled.");
+		emit_signal("remove_ads_purchase_failed");
+		return;
+	}
+
+	if (_remove_ads_product != nullptr) {
+		SKProduct *product = (__bridge SKProduct *)_remove_ads_product;
+		SKPayment *payment = [SKPayment paymentWithProduct:product];
+		[[SKPaymentQueue defaultQueue] addPayment:payment];
+		return;
+	}
+
+	if (_loading_remove_ads_product) {
+		return;
+	}
+
+	_loading_remove_ads_product = true;
+	dispatch_async(dispatch_get_main_queue(), ^{
+		NSSet<NSString *> *productIDs = [NSSet setWithObject:kFlappyAdsRemoveAdsProductID];
+		SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:productIDs];
+		request.delegate = (__bridge FlappyAdsDelegate *)_delegate;
+		_product_request = (__bridge_retained void *)request;
+		[request start];
+	});
+}
+
+void FlappyAdsIOS::restore_purchases() {
+	if (_remove_ads_owned) {
+		emit_signal("remove_ads_purchased");
+		return;
+	}
+
+	dispatch_async(dispatch_get_main_queue(), ^{
+		[[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+	});
+}
+
+bool FlappyAdsIOS::has_remove_ads() const {
+	return _remove_ads_owned;
 }
 
 void FlappyAdsIOS::_on_interstitial_loaded(void *p_interstitial) {
@@ -159,15 +284,86 @@ void FlappyAdsIOS::_on_interstitial_shown() {
 	emit_signal("interstitial_shown");
 }
 
-FlappyAdsIOS::FlappyAdsIOS() {
-	FlappyAdsDelegate *delegate = [[FlappyAdsDelegate alloc] initWithOwner:this];
-	_delegate = (__bridge_retained void *)delegate;
+void FlappyAdsIOS::_on_remove_ads_products_response(void *p_products) {
+	_loading_remove_ads_product = false;
+	if (_product_request != nullptr) {
+		CFRelease(_product_request);
+		_product_request = nullptr;
+	}
+
+	NSArray<SKProduct *> *products = (__bridge NSArray<SKProduct *> *)p_products;
+	SKProduct *matched_product = nil;
+	for (SKProduct *product in products) {
+		if ([product.productIdentifier isEqualToString:kFlappyAdsRemoveAdsProductID]) {
+			matched_product = product;
+			break;
+		}
+	}
+
+	if (!matched_product) {
+		NSLog(@"FlappyAds: StoreKit product not found: %@", kFlappyAdsRemoveAdsProductID);
+		emit_signal("remove_ads_purchase_failed");
+		return;
+	}
+
+	if (_remove_ads_product != nullptr) {
+		CFRelease(_remove_ads_product);
+	}
+	_remove_ads_product = (__bridge_retained void *)matched_product;
+	SKPayment *payment = [SKPayment paymentWithProduct:matched_product];
+	[[SKPaymentQueue defaultQueue] addPayment:payment];
 }
 
-FlappyAdsIOS::~FlappyAdsIOS() {
+void FlappyAdsIOS::_on_remove_ads_product_request_failed() {
+	_loading_remove_ads_product = false;
+	if (_product_request != nullptr) {
+		CFRelease(_product_request);
+		_product_request = nullptr;
+	}
+	emit_signal("remove_ads_purchase_failed");
+}
+
+void FlappyAdsIOS::_on_remove_ads_purchased() {
+	_remove_ads_owned = true;
 	if (_interstitial != nullptr) {
 		CFRelease(_interstitial);
 		_interstitial = nullptr;
+	}
+	emit_signal("remove_ads_purchased");
+}
+
+void FlappyAdsIOS::_on_remove_ads_purchase_failed() {
+	emit_signal("remove_ads_purchase_failed");
+}
+
+void FlappyAdsIOS::_on_remove_ads_purchase_cancelled() {
+	emit_signal("remove_ads_purchase_cancelled");
+}
+
+FlappyAdsIOS::FlappyAdsIOS() {
+	FlappyAdsDelegate *delegate = [[FlappyAdsDelegate alloc] initWithOwner:this];
+	_delegate = (__bridge_retained void *)delegate;
+	[[SKPaymentQueue defaultQueue] addTransactionObserver:delegate];
+}
+
+FlappyAdsIOS::~FlappyAdsIOS() {
+	if (_delegate != nullptr) {
+		FlappyAdsDelegate *delegate = (__bridge FlappyAdsDelegate *)_delegate;
+		[[SKPaymentQueue defaultQueue] removeTransactionObserver:delegate];
+	}
+	if (_interstitial != nullptr) {
+		CFRelease(_interstitial);
+		_interstitial = nullptr;
+	}
+	if (_product_request != nullptr) {
+		SKProductsRequest *request = (__bridge SKProductsRequest *)_product_request;
+		request.delegate = nil;
+		CFRelease(_product_request);
+		_product_request = nullptr;
+	}
+	if (_remove_ads_product != nullptr) {
+		CFRelease(_remove_ads_product);
+		_remove_ads_product = nullptr;
 	}
 	if (_delegate != nullptr) {
 		CFRelease(_delegate);
